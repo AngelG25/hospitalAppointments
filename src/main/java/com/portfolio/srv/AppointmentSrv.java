8e480785f03c8f21e0c0b1540b151784b984576a
INDRA\agcortes <agcortes@eservicios.indra.es>
2025-03-28 13:03:02 +0100

    remove appointment management

--- src/main/java/com/portfolio/srv/AppointmentSrv.java
+++ src/main/java/com/portfolio/srv/AppointmentSrv.java
@@ -19,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.annotation.ApplicationScope;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 @Transactional
@@ -26,6 +28,11 @@ import java.util.List;
 @ApplicationScope
 @Log4j2
 public class AppointmentSrv implements AppointmentApi {
+
+  public static final String APPOINTMENT_WITH_ID = "Appointment with id ";
+  public static final String NOT_FOUND = " not found";
+  public static final String PATIENT_WITH_ID = "Patient with id ";
+  public static final String DOCTOR_WITH_ID = "Doctor with id ";
 
   private final AppointmentRepository appointmentRepository;
   private final AppointmentMapper appointmentMapper;
@@ -44,7 +51,7 @@ public class AppointmentSrv implements AppointmentApi {
   public void updateAppointment(Appointment appointment) {
     if (appointmentRepository.findById(appointment.getIdAppointment())
         .isEmpty()) {
-      throw new AppointmentNotFoundException("Appointment with id " + appointment.getIdAppointment() + " not found");
+      throw new AppointmentNotFoundException(APPOINTMENT_WITH_ID + appointment.getIdAppointment() + NOT_FOUND);
     }
     checkDoctorPatientExistence(appointment);
     AppointmentDao appointmentDao = appointmentMapper.toDao(appointment);
@@ -56,7 +63,7 @@ public class AppointmentSrv implements AppointmentApi {
   public Appointment getAppointmentById(String id) {
     return appointmentRepository.findById(id)
         .map(appointmentMapper::toDto)
-        .orElseThrow(() -> new AppointmentNotFoundException("Appointment with id " + id + " not found"));
+        .orElseThrow(() -> new AppointmentNotFoundException(APPOINTMENT_WITH_ID + id + NOT_FOUND));
   }
 
   @Override
@@ -69,8 +76,9 @@ public class AppointmentSrv implements AppointmentApi {
 
   @Override
   public void deleteAppointment(String id) {
-    appointmentRepository.findById(id)
-        .orElseThrow(() -> new AppointmentNotFoundException("Appointment with id " + id + " not found"));
+    AppointmentDao appointmentDao = appointmentRepository.findById(id)
+        .orElseThrow(() -> new AppointmentNotFoundException(APPOINTMENT_WITH_ID + id + NOT_FOUND));
+    removeAppointments(appointmentDao);
     appointmentRepository.deleteById(id);
   }
 
@@ -92,10 +100,10 @@ public class AppointmentSrv implements AppointmentApi {
 
   private void checkDoctorPatientExistence(Appointment appointment) {
     if (!patientRepository.existsById(appointment.getIdPatient())) {
-      throw new PatientNotFoundException("Patient with id " + appointment.getIdPatient() + " not found");
+      throw new PatientNotFoundException(PATIENT_WITH_ID + appointment.getIdPatient() + NOT_FOUND);
     }
     if (!doctorRepository.existsById(appointment.getIdDoctor())) {
-      throw new DoctorNotFoundException("Doctor with id " + appointment.getIdDoctor() + " not found");
+      throw new DoctorNotFoundException(DOCTOR_WITH_ID + appointment.getIdDoctor() + NOT_FOUND);
     }
   }
 
@@ -106,17 +114,48 @@ public class AppointmentSrv implements AppointmentApi {
 
   private void assignAppointmentDoctor(Appointment appointment, AppointmentDao appointmentDao) {
     DoctorDao doctorDao = doctorRepository.findById(appointment.getIdDoctor())
-        .orElseThrow(
-            () -> new DoctorNotFoundException("Doctor with id: " + appointment.getIdDoctor() + " not found"));
+        .orElseThrow(() -> new DoctorNotFoundException(DOCTOR_WITH_ID + appointment.getIdDoctor() + NOT_FOUND));
     doctorDao.getAppointments().add(appointmentDao);
     doctorRepository.save(doctorDao);
   }
 
   private void assignAppointmentPatient(Appointment appointment, AppointmentDao appointmentDao) {
     PatientDao patientDao = patientRepository.findById(appointment.getIdPatient())
-        .orElseThrow(
-            () -> new PatientNotFoundException("Patient with id: " + appointment.getIdPatient() + " not found"));
+        .orElseThrow(() -> new PatientNotFoundException(PATIENT_WITH_ID + appointment.getIdPatient() + NOT_FOUND));
     patientDao.getAppointments().add(appointmentDao);
     patientRepository.save(patientDao);
+  }
+
+  private void removeAppointments(AppointmentDao appointmentDao) {
+    removeDoctorAppointment(appointmentDao);
+    removePatientAppointment(appointmentDao);
+  }
+
+  private void removePatientAppointment(AppointmentDao appointmentDao) {
+    PatientDao patientDao = patientRepository.findById(appointmentDao.getIdPatient())
+        .orElseThrow(() -> new PatientNotFoundException(PATIENT_WITH_ID + appointmentDao.getIdPatient() + NOT_FOUND));
+    Optional<AppointmentDao> appointmentToRemove = patientDao.getAppointments()
+        .stream()
+        .filter(patientAppointment -> patientAppointment.getIdAppointment()
+            .equals(appointmentDao.getIdAppointment()))
+        .findFirst();
+
+    appointmentToRemove.ifPresent(a -> patientDao.getAppointments()
+        .remove(a));
+    patientRepository.save(patientDao);
+  }
+
+  private void removeDoctorAppointment(AppointmentDao appointmentDao) {
+    DoctorDao doctorDao = doctorRepository.findById(appointmentDao.getIdDoctor())
+        .orElseThrow(() -> new DoctorNotFoundException(DOCTOR_WITH_ID + appointmentDao.getIdDoctor() + NOT_FOUND));
+    Optional<AppointmentDao> appointmentToRemove = doctorDao.getAppointments()
+        .stream()
+        .filter(doctorAppointment -> doctorAppointment.getIdAppointment()
+            .equals(appointmentDao.getIdAppointment()))
+        .findFirst();
+
+    appointmentToRemove.ifPresent(a -> doctorDao.getAppointments()
+        .remove(a));
+    doctorRepository.save(doctorDao);
   }
 }
